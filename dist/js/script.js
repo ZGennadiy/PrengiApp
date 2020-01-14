2cd2dc6bd065de489214c71f6a91e62d0b7a3f73
ZGennady <zgennadiy.dev@gmail.com>
2020-01-14 23:28:11 +0300

    set js for sliders, bullets and minor fixes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,4 @@
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -10,17 +10,17 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slider__item");
-  let bullets = document.getElementsByClassName("slider__bullet");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  var i;
+  var slides = document.getElementsByClassName("slider__item");
+  var bullets = document.getElementsByClassName("slider__bullet");
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
+  for (i = 0; i < slides.length; i += 1) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < bullets.length; i += 1) {
-      bullets[i].className = bullets[i].className.replace("slider__bullet_active", "");
+      bullets[i].className = bullets[i].className.replace(" slider__bullet_active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  bullets[slideIndex-1].className += "slider__bullet_active";
+  bullets[slideIndex-1].className += " slider__bullet_active";
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -10,9 +10,10 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slider__item");
-  var bullets = document.getElementsByClassName("slider__bullet");
+  let i;
+  let slides = document.getElementsByClassName("slider__item");
+  let bullets = document.getElementsByClassName("slider__bullet");
+  let tabs = document.getElementsByClassName("slider__tab");
   if (n > slides.length) {slideIndex = 1;}
   if (n < 1) {slideIndex = slides.length;}
   for (i = 0; i < slides.length; i += 1) {
@@ -21,6 +22,10 @@ function showSlides(n) {
   for (i = 0; i < bullets.length; i += 1) {
       bullets[i].className = bullets[i].className.replace(" slider__bullet_active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  bullets[slideIndex-1].className += " slider__bullet_active";
+  for (i = 0; i < tabs.length; i += 1) {
+      tabs[i].className = tabs[i].className.replace(" slider__tab_active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  bullets[slideIndex - 1].className += " slider__bullet_active";
+  tabs[slideIndex - 1].className += " slider__tab_active";
 }
